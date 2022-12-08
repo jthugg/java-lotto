d113ecbd9009a44a7d05bd13473b4e2fb137c811
jthugg <jthug@naver.com>
2022-12-08 23:43:00 +0900

    Fix: 테스트 수정, 보너스 번호 까지 7개를 저장하는 방식 사용

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -9,10 +9,10 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
-    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
+    @DisplayName("로또 번호의 개수가 7개가 넘어가면 예외가 발생한다.")
     @Test
     void createLottoByOverSize() {
-        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7, 8)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
